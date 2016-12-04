98030059a997ce2764aa49474229ac9de01ac2e0
milanerski <a@d.c>
2016-12-04 13:30:42 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>FORMA AJAX - PRIMER</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+
+    <link rel="stylesheet" href="css/fax.css">
+    <script src="./js/jquery.min.js"></script>
+    <script src="./js/bootstrap.min.js"></script>
+</head>
+<body>
+<!-- tranzicija od kvadrata da se pravi krug, pa se vraca u kvadrat, sa promenom boje -->
+
+
+
+<div class="container col-lg-12" id="kutijica">
+
+
+
+
+</div>
+
+
+
+
+
+
+</body>
+</html>
